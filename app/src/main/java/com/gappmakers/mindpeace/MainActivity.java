d458b2d232e68326683891b8d661b0a062914fa9
chyan0411 <chyan0411@users.noreply.github.com>
2018-06-08 16:32:55 +0200

    first tabs and drawer

--- app/src/main/java/com/gappmakers/mindpeace/MainActivity.java
+++ app/src/main/java/com/gappmakers/mindpeace/MainActivity.java
@@ -1,13 +1,35 @@
 package com.gappmakers.mindpeace;
 
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //Add Fragments
+        adapter.AddFragment(new FragmentHome(),"Home");
+        adapter.AddFragment(new FragmentTrip(),"Trip");
+        adapter.AddFragment(new FragmentMore(),"More");
+        //Add setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }}
---
+++ app/src/main/java/com/gappmakers/mindpeace/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.gappmakers.mindpeace;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment>fragmentList = new ArrayList<>();
+    private  final  List<String>FragmentListTitles = new ArrayList<>();
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return FragmentListTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return FragmentListTitles.get(position);
+    }
+    public void AddFragment(FragmentTrip fragment, String Title){
+        fragmentList.add(fragment);
+        FragmentListTitles.add(Title);
+
+    }
+    public void AddFragment(FragmentHome fragment, String Title){
+        fragmentList.add(fragment);
+        FragmentListTitles.add(Title);
+
+    }
+    public void AddFragment(FragmentMore fragment, String Title){
+        fragmentList.add(fragment);
+        FragmentListTitles.add(Title);
+
+    }
+
+}
