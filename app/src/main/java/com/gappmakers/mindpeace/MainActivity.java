faf17f372b46b7a890703f3d0ddb73d49a77d112
mortalrahu <32536997+mortalrahu@users.noreply.github.com>
2018-06-12 13:25:27 +0200

    changes #1

--- app/src/main/java/com/gappmakers/mindpeace/MainActivity.java
+++ app/src/main/java/com/gappmakers/mindpeace/MainActivity.java
@@ -1,10 +1,13 @@
 package com.gappmakers.mindpeace;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
-
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    FragmentNotification n = new FragmentNotification(this);
+    FragmentBefo b = new FragmentBefo(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +35,57 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
         TABAdapter adapter = new TABAdapter(getSupportFragmentManager());
         //Add Fragments
+        adapter.AddFragment(new FragmentNotification(),"Home");
         adapter.AddFragment(new FragmentBefo(),"BEFO");
         adapter.AddFragment(new FragmentFollo(),"FOLLO");
-        adapter.AddFragment(new FragmentNotification(),"Notification");
+
         //Add setup
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+       mDrawerLayout= (DrawerLayout)findViewById(R.id.drawerlayout_id);
+       mToggle= new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close );
+       mDrawerLayout.addDrawerListener(mToggle);
+       mToggle.syncState();
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final NavigationView navigation = (NavigationView) findViewById(R.id.nav_view);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                switch(item.getItemId()) {
+                    case R.id.nav_home:
+                        return true;
+                    case R.id.nav_befo:
+                        return true;
+                    case R.id.nav_follo:
+                        return true;
+                    case R.id.nav_setting:
+                        return true;
+                    case R.id.nav_delete:
+                        return true;
+                    case R.id.nav_about:
+                        return true;
+                    default:
+                        return false;
+                }
 
+              //  if (id == R.id.nav_home) {
+               //     startActivity(new Intent(, FragmentNotification.class));
+               // } else if (id == R.id.nav_befo) {
+               //     startActivity(new Intent(this, FragmentBefo.class));
+               // }
+            }
+        });
     }
-    public void NewTrip(View view) {
+
+        public boolean onOptionsItemSelected (MenuItem item){
+
+            if(mToggle.onOptionsItemSelected(item)){return true;}
+            return super.onOptionsItemSelected(item);
+        }
+        public void NewTrip(View view) {
         Intent newTrip = new Intent(this,NewTrip.class);
         startActivity(newTrip);
         }
 
-        public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.drawer_view,menu);
-        return true;
-        }
-
-        public boolean onOptionsItemSelected (MenuItem item){
-
-        switch(item.getItemId()){
-            case R.id.nav_befo:
-                Intent intent = new Intent(this,NewTrip.class);
-                startActivity(intent);
-        }
-//        switch(item.getItemId()){
-//                case R.id.nav_follo:
-//                    Intent intent = new Intent(this,NewTrip.class);
-//                    startActivity(intent);
-//            }
-
-
-        return super.onOptionsItemSelected(item);
-        }
 }
