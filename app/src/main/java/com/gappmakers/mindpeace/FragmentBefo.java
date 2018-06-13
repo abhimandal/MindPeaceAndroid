035012c622f602491188d4b78c2e34f4aa53a776
chyan0411 <chyan0411@users.noreply.github.com>
2018-06-13 18:17:25 +0200

    slight change

--- app/src/main/java/com/gappmakers/mindpeace/FragmentBefo.java
+++ app/src/main/java/com/gappmakers/mindpeace/FragmentBefo.java
@@ -1,5 +1,7 @@
 package com.gappmakers.mindpeace;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,13 +15,15 @@ import android.widget.Button;
 
 public class FragmentBefo extends Fragment {
     View view;
+    Context c;
     public FragmentBefo() {
 
     }
-    @Nullable
-    @Override
+    @SuppressLint("ValidFragment")
 
-
+    public FragmentBefo(Context context) {
+        c=context;
+    }
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.befo_fragment,container,false);
         return view;
